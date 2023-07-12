8e4e49a73b1c7380c731b5a4c9a4fe23b779d791
Dat <dreamtran2211@icloud.com>
2023-07-12 16:10:04 -0400

    fixed

--- website/models.py
+++ website/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django import forms
 from django.contrib.auth.models import User
+
 # Create your models here.
 class Record(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
